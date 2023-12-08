2f370c616a57dfe15cf648fb314c5b76dfef01ab
hadirmanai <hadilmanai08@gmail.com>
2023-12-08 04:57:22 +0100

    FinalUpdate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'myproject';
+  private localStorageKey = 'authToken';
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  login(username: string, password: string): void {
+    // Appel à l'API pour la connexion avec l'email et le mot de passe
+    this.http.post<any>('localhost:8080/realms/springdev-realm/protocol/openid-connect/token', { username, password })
+      .subscribe(
+        (response) => {
+          console.log(response)
+          // Récupération du token depuis la réponse de l'API
+          const authToken = response.access_token;
+          console.log(authToken)
+
+          // Stockage du token dans le localStorage
+          localStorage.setItem(this.localStorageKey, authToken);
+
+          // Redirection vers le tableau de bord après la connexion réussie
+          this.router.navigate(['/DashbordAdmin']);
+        },
+        (error) => {
+          // Gérer les erreurs lors de la connexion (par exemple, afficher un message d'erreur)
+          console.error('Erreur de connexion : ', error);
+        }
+      );
+  }
 }
